8c2ed3c783529ba3e5f4196cafd5fb6bca72d30b
josimarbs83-dev <josimar.bs83@gmail.com>
2026-08-12 00:03:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-
-module.exports = (req, res) => {
-  const filePath = path.join(process.cwd(), "index.html");
-
-  fs.readFile(filePath, "utf8", (err, html) => {
-    if (err) {
-      res.statusCode = 500;
-      return res.end("Erro ao carregar o DESLIGA.");
-    }
-
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.statusCode = 200;
-    res.end(html);
-  });
-};
+export default function handler(req, res) {
+  res.status(404).end();
+}
